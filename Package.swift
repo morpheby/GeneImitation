a84fc6b7e2970fb6c138bb0a73803c9131da0365
Ilya Mikhaltsou <morpheby@gmail.com>
2017-07-01 02:24:22 -0700

    Added swiftz

--- Package.swift
+++ Package.swift
@@ -7,5 +7,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/morpheby/Algorithm.git", "2.0.4-dev"),
         .Package(url: "https://github.com/mxcl/PromiseKit.git", "5.0.0-alpha"),
+        .Package(url: "https://github.com/typelift/Swiftz.git", majorVersion: 0, minor: 6),
     ]
 )
