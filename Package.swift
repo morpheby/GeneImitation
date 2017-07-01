8b3767c23600e52f60cdd2fe78bc945829b23e73
Ilya Mikhaltsou <morpheby@gmail.com>
2017-07-01 02:27:13 -0700

    Name change

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "gene-imitation",
+    name: "GeneImitation",
     dependencies: [
         .Package(url: "https://github.com/morpheby/Algorithm.git", "2.0.4-dev"),
         .Package(url: "https://github.com/mxcl/PromiseKit.git", "5.0.0-alpha"),
