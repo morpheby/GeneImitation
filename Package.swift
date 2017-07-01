e376e1b70448ff7ae0c8054f691d9503a1064b4d
Ilya Mikhaltsou <morpheby@gmail.com>
2017-07-01 01:29:10 -0700

    Added PromiseKit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "gene-imitation",
     dependencies: [
-        .Package(url: "https://github.com/morpheby/Algorithm.git", majorVersion: 2),
+        .Package(url: "https://github.com/morpheby/Algorithm.git", "2.0.4-dev"),
+        .Package(url: "https://github.com/mxcl/PromiseKit.git", "5.0.0-alpha"),
     ]
 )
